d8dd30149355e483bd5200a6f68ff3c43da59d72
Arthur MACDONALD <arthur.macdonald@etu.u-bordeaux.fr>
2023-12-07 20:32:26 +0100

    Added new components

---
+++ src/app/debunk-page/debunk-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-debunk-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './debunk-page.component.html',
+  styleUrl: './debunk-page.component.css'
+})
+export class DebunkPageComponent {
+
+}
