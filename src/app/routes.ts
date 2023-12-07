857f89f15ea3c7408d7e503c0ac37a334bcff118
Guilhem <bonnefousguilhem@gmail.com>
2023-12-08 00:43:42 +0100

    Add font-Inter + made about page + positionned navbar and footer

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
@@ -8,6 +9,11 @@ const routeConfig: Routes = [
       path: '',
       component: HomeComponent,
       title: 'Home page'
+    },
+    {
+      path: 'about',
+      component: AboutComponent,
+      title: 'About page'
     },
     {
       path: '**',
