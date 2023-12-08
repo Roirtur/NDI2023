24d53ebab5731e1c10c58d47dd44e871c9840945
Arthur MACDONALD <arthur.macdonald@etu.u-bordeaux.fr>
2023-12-08 04:18:46 +0100

    Fixed bugs & all

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -26,13 +26,27 @@ export class NavbarComponent implements OnInit {
         jQuery('.navigation-buttons').css({
           'margin-top' : - scrollPos + "px",
         });
+        jQuery('.navigation-corner,.navigation-title').css({
+          'display' : 'none',
+        });
       } 
       
+      if (scrollPos < (jQuery('.navigation-container').height() || 0) - 300){
+        jQuery('.navigation-corner,.navigation-title').css({
+          'display' : 'block',
+        });
+      }
       if (scrollPos < (jQuery('.navigation-container').height() || 0) - 700){
         jQuery('.classic-navigation').fadeOut(300);
+        jQuery('.navigation-container').css({
+          'background-color' : '#000',
+        });
       }
       else {
         jQuery('.classic-navigation').fadeIn(300);
+        jQuery('.navigation-container').css({
+          'background-color' : '#ECE3CE',
+        });
       }
     });  
 
