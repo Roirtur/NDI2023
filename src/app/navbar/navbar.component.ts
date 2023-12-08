180f8c91944695694c5694275f7b1d3640f73316
Arthur MACDONALD <arthur.macdonald@etu.u-bordeaux.fr>
2023-12-08 05:16:56 +0100

    fixed things

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -18,40 +18,20 @@ export class NavbarComponent implements OnInit {
 
     jQuery('.classic-navigation').fadeOut(0);
 
-    jQuery(document).on('init', function(){
-      console.log('init');
-      const scrollPos = jQuery(this).scrollTop() || 0;
-      if (scrollPos < (jQuery('.navigation-container').height() || 0)) {
-        jQuery('.navigation-corner,.navigation-title').css({
-          'margin-left' : - scrollPos*2 + "px",
-        });
-        jQuery('.navigation-buttons').css({
-          'margin-top' : - scrollPos + "px",
-        });
-        jQuery('.navigation-corner,.navigation-title').css({
-          'display' : 'none',
-        });
-      } 
-      
-      if (scrollPos < (jQuery('.navigation-container').height() || 0) - 300){
-        jQuery('.navigation-corner,.navigation-title').css({
-          'display' : 'block',
-        });
-      }
-      if (scrollPos < (jQuery('.navigation-container').height() || 0) - 700){
-        jQuery('.classic-navigation').fadeOut(300);
-        jQuery('.navigation-container').css({
-          'background-color' : '#000',
-        });
-      }
-      else {
-        jQuery('.classic-navigation').fadeIn(300);
-        jQuery('.navigation-container').css({
-          'background-color' : '#ECE3CE',
-        });
-      }
-    });
-
+    const scrollPos = jQuery(document).scrollTop() || 0;
+    console.log((scrollPos < (jQuery('.navigation-container').height() || 0) + 300));
+    if (scrollPos < (jQuery('.navigation-container').height() || 0) + 300 && scrollPos != 0) {
+      jQuery('.navigation-corner,.navigation-title').css({
+        'margin-left' : - scrollPos*2 + "px",
+      });
+      jQuery('.navigation-buttons').css({
+        'margin-top' : - scrollPos + "px",
+      });
+      jQuery('.navigation-corner,.navigation-title').css({
+        'display' : 'none',
+      });
+    } 
+  
     jQuery(document).on('scroll', function(){
       const scrollPos = jQuery(this).scrollTop() || 0;
       if (scrollPos < (jQuery('.navigation-container').height() || 0)) {
