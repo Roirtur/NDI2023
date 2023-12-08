9f8d959fb06865741598fea8960713487e09b985
Arthur MACDONALD <arthur.macdonald@etu.u-bordeaux.fr>
2023-12-08 03:16:07 +0100

    Added majority of navigation bar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +9,31 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
-  veryImportantFunction() {
-    var audio = new Audio('../../assets/gragas.ogg');
-    audio.play();
-  }
 
+export class NavbarComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {
+
+    jQuery('.classic-navigation').fadeOut(0);
+
+    jQuery(document).on('scroll', function(){
+      const scrollPos = jQuery(this).scrollTop() || 0;
+      if (scrollPos < (jQuery('.navigation-container').height() || 0)) {
+        jQuery('.navigation-corner,.navigation-title').css({
+          'margin-left' : - scrollPos*2 + "px",
+        });
+        jQuery('.navigation-buttons').css({
+          'margin-top' : - scrollPos + "px",
+        });
+      } 
+      
+      if (scrollPos < (jQuery('.navigation-container').height() || 0) - 700){
+        jQuery('.classic-navigation').fadeOut(300);
+      }
+      else {
+        jQuery('.classic-navigation').fadeIn(300);
+      }
+    });  
+
+  }
 }
