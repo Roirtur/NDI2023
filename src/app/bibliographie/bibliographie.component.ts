1c356aad75041db055875b3e61a8a9ca751a08d4
Tourlat <foucaultfelix33@gmail.com>
2023-12-08 04:51:13 +0100

    Updated error page

---
+++ src/app/bibliographie/bibliographie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bibliographie',
+  standalone: true,
+  imports: [],
+  templateUrl: './bibliographie.component.html',
+  styleUrl: './bibliographie.component.css'
+})
+export class BibliographieComponent {
+
+}
